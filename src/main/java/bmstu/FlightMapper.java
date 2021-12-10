6ad0d205ce2d64723e27823c36b73e6e1aa7c3d0
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:17:22 +0300

    Scripted auto-commit on change (2021-12-10 18:17:22) by gitwatch.sh

--- src/main/java/bmstu/FlightMapper.java
+++ src/main/java/bmstu/FlightMapper.java
@@ -19,7 +19,8 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
         if (key.get()>0) {
             String delay = values[DELAY_POSITION];
             String airportId = values[AIRPORT_ID_POSITION];
-            
+            boolean delayCheck = ()
+
 
         }
     }
