1a469460ef9b3ae63e6004bbeb8adbea86d3961d
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:54:00 +0300

    Scripted auto-commit on change (2021-12-10 18:54:00) by gitwatch.sh

--- src/main/java/bmstu/AirportReducer.java
+++ src/main/java/bmstu/AirportReducer.java
@@ -13,7 +13,7 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
         float delayMin = Float.MAX_VALUE;
         int flightCount = 0;
         float delaySum = 0.0f;
-        Iterator<Text> iter = .iterator();
+        Iterator<Text> iter = values.iterator();
         if (iter.hasNext()) {
             Text airportName = new Text(iter.next());
 
