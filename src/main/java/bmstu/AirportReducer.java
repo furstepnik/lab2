296543ad3c332837fe4ec38f636c3afba4dcb733
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:45:11 +0300

    Scripted auto-commit on change (2021-12-10 18:45:11) by gitwatch.sh

--- src/main/java/bmstu/AirportReducer.java
+++ src/main/java/bmstu/AirportReducer.java
@@ -2,11 +2,14 @@ package bmstu;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
     @Override
     protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
+        
+    }
 
 
 }
