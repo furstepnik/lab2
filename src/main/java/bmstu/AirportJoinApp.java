8a3f495fb48b72d9ff27d113ff07cd805455cb84
furstepnik <furstepnik@yandex.ru>
2021-12-10 19:23:06 +0300

    Scripted auto-commit on change (2021-12-10 19:23:06) by gitwatch.sh

--- src/main/java/bmstu/AirportJoinApp.java
+++ src/main/java/bmstu/AirportJoinApp.java
@@ -1,13 +1,13 @@
 package bmstu;
 
+import java.io.IOException;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-
-import java.io.IOException;
 
 
 public class AirportJoinApp {
