cc1cb77746d3198e7642463b693fd873c016e861
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:08:42 +0300

    Scripted auto-commit on change (2021-12-10 18:08:42) by gitwatch.sh

--- src/main/java/bmstu/FlightMapper.java
+++ src/main/java/bmstu/FlightMapper.java
@@ -4,6 +4,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    
+    @Override
+    protected void map(LongWritable key, Text value,
+                       Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
+            throws IOException, InterruptedException {
+        
+    }
 }
