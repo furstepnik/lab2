55ea4a5d936d8c72ae32364864ed05801aed79bd
furstepnik <furstepnik@yandex.ru>
2021-12-10 19:12:45 +0300

    Scripted auto-commit on change (2021-12-10 19:12:45) by gitwatch.sh

--- src/main/java/bmstu/AirportReducer.java
+++ src/main/java/bmstu/AirportReducer.java
@@ -28,7 +28,7 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
                 flightCount++;
             }
             if (flightCount>0) {
-                String airport = "\n"
+                String airport = "\nAirport" 
             }
 
     }
