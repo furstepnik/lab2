cde475860166714e88e6fede08a04d0d07434e35
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:52:02 +0300

    Scripted auto-commit on change (2021-12-10 18:52:02) by gitwatch.sh

--- src/main/java/bmstu/AirportReducer.java
+++ src/main/java/bmstu/AirportReducer.java
@@ -3,6 +3,7 @@ package bmstu;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class AirportReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
     @Override
@@ -12,7 +13,7 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
         float delayMin = Float.MAX_VALUE;
         int flightCount = 0;
         float delaySum = 0.0f;
-        
+        Iterator
 
     }
 
