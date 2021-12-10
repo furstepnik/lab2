c4fa8e7d1deee5556ab89fc8863ee69727fd1a19
furstepnik <furstepnik@yandex.ru>
2021-12-10 17:41:26 +0300

    Scripted auto-commit on change (2021-12-10 17:41:26) by gitwatch.sh

--- src/main/java/bmstu/AirportsMapper.java
+++ src/main/java/bmstu/AirportsMapper.java
@@ -2,8 +2,8 @@ package bmstu;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text>{
-
+    
 }
