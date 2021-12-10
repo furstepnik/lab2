db4771ca5539d32467e7f2ab0f14a2a22477f24c
furstepnik <furstepnik@yandex.ru>
2021-12-10 16:03:37 +0300

    Scripted auto-commit on change (2021-12-10 16:03:37) by gitwatch.sh

--- src/main/java/bmstu/AirportJoinApp.java
+++ src/main/java/bmstu/AirportJoinApp.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
