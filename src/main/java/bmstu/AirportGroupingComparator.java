5140bfb578f20ddd2b765a61fb1b2b5f0f3424a6
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:33:49 +0300

    Scripted auto-commit on change (2021-12-10 18:33:49) by gitwatch.sh

--- src/main/java/bmstu/AirportGroupingComparator.java
+++ src/main/java/bmstu/AirportGroupingComparator.java
@@ -4,7 +4,9 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
     public AirportGroupingComparator() {
-        super(AirportWritableComparable.class )
+        super(AirportWritableComparable.class, true);
     }
+
+    
 
 }
