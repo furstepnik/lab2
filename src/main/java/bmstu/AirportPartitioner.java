0e972e75002515e00661d65fad2277a048c72b2f
furstepnik <furstepnik@yandex.ru>
2021-12-10 18:28:45 +0300

    Scripted auto-commit on change (2021-12-10 18:28:45) by gitwatch.sh

--- src/main/java/bmstu/AirportPartitioner.java
+++ src/main/java/bmstu/AirportPartitioner.java
@@ -1,7 +1,6 @@
 package bmstu;
 
 import org.apache.hadoop.mapreduce.Partitioner;
-
 import javax.xml.soap.Text;
 
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
