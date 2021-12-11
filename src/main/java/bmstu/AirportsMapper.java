b4ca6e6f22bddcadb7e06e8f87497fd1e391862d
furstepnik <furstepnik@yandex.ru>
2021-12-11 14:16:30 +0300

    Scripted auto-commit on change (2021-12-11 14:16:30) by gitwatch.sh

--- src/main/java/bmstu/AirportsMapper.java
+++ src/main/java/bmstu/AirportsMapper.java
@@ -7,16 +7,28 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportsMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text>{
+
+    public static final int ID = 0;
+    public static final int DESCRIPTION = 1;
+    public static final int AIRPORT_FLAG = 0;
+    public static final String SEPARATOR = "\",\"";
+    public static final String REGEX_REPLACE = "\"";
+
     @Override
     protected void map(LongWritable key, Text value,
                        Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
             throws IOException, InterruptedException {
         String table = value.toString();
-        String[] values = table.split("\",\"");
+        String[] values = table.split(SEPARATOR);
         if (key.get() > 0) {
-            int airportId = Integer.parseInt(values[0].replaceAll("\"", ""));
-            String airportName = values[1].replaceAll("\"", "");
-            context.write(new AirportWritableComparable(airportId, 0), new Text(airportName));
+            int airportId = Integer.parseInt(deleteQuotes(values[ID]));
+            String airportName = deleteQuotes(values[DESCRIPTION]);
+            context.write(new AirportWritableComparable(airportId, AIRPORT_FLAG), new Text(airportName));
         }
+    }
+
+
+    private String deleteQuotes(String str) {
+        return str.replaceAll(REGEX_REPLACE, "");
     }
 }
--- src/main/java/bmstu/FlightMapper.java
+++ src/main/java/bmstu/FlightMapper.java
@@ -10,19 +10,23 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
 
     public static final int DELAY_POSITION = 18;
     public static final int AIRPORT_ID_POSITION = 14;
+    public static final int CANCELLED_STATUS_POSITION = 19;
+    public static final float CANCELLED = 1.0f;
+    public static final int FLIGHT_FLAG = 1;
+    public static final String SEPARATOR = ",";
     @Override
     protected void map(LongWritable key, Text value,
                        Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
             throws IOException, InterruptedException {
         String table = value.toString();
-        String[] values = table.split(",");
+        String[] values = table.split(SEPARATOR);
         if (key.get()>0) {
             String delay = values[DELAY_POSITION];
             int airportId = Integer.parseInt(values[AIRPORT_ID_POSITION]);
-            boolean delayCheck = (Float.parseFloat(values[DELAY_POSITION+1])==1.0f);
+            boolean delayCheck = (Float.parseFloat(values[CANCELLED_STATUS_POSITION])==CANCELLED);
             if (!delayCheck) {
                 if ((!delay.isEmpty()) && (Float.parseFloat(delay)>0f)) {
-                    context.write(new AirportWritableComparable(airportId, 1), new Text(values[DELAY_POSITION]));
+                    context.write(new AirportWritableComparable(airportId, FLIGHT_FLAG), new Text(values[DELAY_POSITION]));
                 }
             }
         }
